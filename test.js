a050cf6c4cfbf2fa8cbd7031aabd8107a9028421
Timothée Maman <maman.timothee@hotmail.fr>
2020-03-18 18:36:40 +0100

    First JS

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        console.log(response.current_condition.condition);
-    }
-};
 
-request.open("GET", "http://localhost:3000/api/cameras");
-request.send();
+let elt= document.getElementById('open');
+
+async function test() {
+response = await fetch("http://localhost:3000/api/cameras");
+data = await response.json()
+.then((data) => { 
+console.log(elt.innerHTML+=data);
+})
+}
+
+
