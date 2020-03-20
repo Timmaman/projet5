817085d83f56b041531b87a0f6af160558a9a80e
Timothée Maman <maman.timothee@hotmail.fr>
2020-03-20 19:00:56 +0100

    JS corrections

--- affichage_produits.js
+++ affichage_produits.js
@@ -1,32 +1,42 @@
 oldname=0
 
-async function test() {
+async function recup() {
 	response = await fetch("http://localhost:3000/api/cameras");
 	data = await response.json()
 	return data; 
 	}
-	test().then(function(data){
+	recup().then(function(data){
 	
 		for (article of data)
 			{
 			//pour créer la div parent de chaque produit
 			if(article.name!=oldname){
-				parent=document.getElementById('open')
-				new_parent=document.createElement("div");
+				parent=document.getElementById('articles')
+				new_parent=document.createElement("a");
+				new_parent.href="produit.html"
 				name=article.name
 				parent.append(new_parent)
 				new_parent.id=article.name
+				new_parent.alt=article.name
 				oldname=name
-				test=article.imageurl
-					console.log(test)
 				//Pour créer les paragraphes avec les infos
 				if (article.name=oldname){
+					//Affichage photo
+					child_photo=document.getElementById(article.name)
+					photo_child=document.createElement("img")
+					photo_child.src=article.imageUrl
+					photo_child.alt=article.name
+					photo_child.title=article.name
+					child_photo.append(photo_child)
+					photo_child.id="image"
+
 					//Pour afficher le nom du produit
 					child_name=document.getElementById(article.name)
 					name_child=document.createElement("p")
 					child_name.append(name_child)
 					name_child.id="name"
 					console.log (name_child.innerHTML+=name)
+
 					//Affichage du prix
 					child_price=document.getElementById(article.name)
 					price_child=document.createElement("p")
